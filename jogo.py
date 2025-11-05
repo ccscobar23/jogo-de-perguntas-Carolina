815b8d13b9233b35be74b993c6375b8f18b7af6d
ccscobar23 <carolina.escobar@escola.pr.gov.br>
2025-11-05 16:53:54 -0300

    jogo.py

--- jogo.py
+++ jogo.py
@@ -1,3 +1,7 @@
 #colocar título e descrever o jogo
 print("JOGO DE PERGUNTAS")
-print("O jogo será trabalhado na forma de utilizar dados externos, usando conceitos de INPUTs para entrada de dados.")
+
+print("O jogo será trabalhado na forma de utilizar dados externos, para desenvolver respostas e novas perguntas usando a ideia de input.")
+#pensar em um tema
+nome = input("qual é seu nome?")
+print("oi", nome)
