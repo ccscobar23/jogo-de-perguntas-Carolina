645ef27be08dc7d830dd29f04ff5d7b457a18169
ccscobar23 <carolina.escobar@escola.pr.gov.br>
2025-11-05 16:40:08 -0300

    Add files via upload

---
+++ jogo.py
@@ -0,0 +1,3 @@
+#colocar título e descrever o jogo
+print("JOGO DE PERGUNTAS")
+print("O jogo será trabalhado na forma de utilizar dados externos, usando conceitos de INPUTs para entrada de dados.")
